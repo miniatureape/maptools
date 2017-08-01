99d0839ab38559062cd7077e0f02052d685d8afd
Justin Donato <justin.donato@gmail.com>
2017-08-01 08:18:58 -0400

    Initial import

---
+++ README.md
@@ -0,0 +1,16 @@
+# Maptools
+
+A tool for doing advance research of a place.
+
+## TODOS
+
+- [ ] Create root app and boostrap index file (create store etc).
+- [ ] Create Exploration Map Component
+- [ ] Create Context Map Component
+- [ ] Add home feature
+- [ ] Add Place
+- [ ] See places feature
+    - [ ] When you look at the list, the distances to home should show along side it.
+- [ ] Remove Place
+- [ ] Toggle Context Map
+
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+     context: __dirname,
+     entry: path.resolve(__dirname, 'src/index.jsx'),
+     output: {
+         path: path.resolve(__dirname, './dist'),
+         filename: 'bundle.js'
+     },
+     resolve: {
+         extensions: [".js", ".json", ".jsx"]
+     },
+     module: {
+     loaders: [{
+            test: /.jsx?$/,
+            include: path.resolve(__dirname, './src'),
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+         }, {
+            test: /\.css$/, loader: "style-loader!css-loader" 
+         }
+         ]
+     },
+     plugins: [
+        new webpack.ProvidePlugin({
+            "React": "react",
+        }),
+        new HtmlWebpackPlugin()
+     ],
+ };
