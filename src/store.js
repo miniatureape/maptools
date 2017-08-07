0cb029790f75989e4a54cf1f436b10e196bceb90
Justin Donato <justin.donato@gmail.com>
2017-08-07 13:53:31 -0400

    Change start location in zoom

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ let store = createStore(appReducers, {
     displayContextMap: false,
     center: {
         lat: 40.7128,
-        lng: 74.0059
+        lng: -74.0059
     },
     bounds: null,
     zoom: 5,
